80e5e285c709fc3a3096c26f1f7ebf729dc60998
ThiagoBettinRamos <thiago_bettin@hotmail.com>
2023-08-01 01:31:50 -0300

    first game finish

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Snake-Game-ThiagoBettinRamos
+# Snake Game 🐍
+
+![Screenshot do Jogo](assets/snake.jpg)
+
+Este é um projeto de Snake Game moderno e responsivo feito com HTML5, CSS3 e JavaScript puro.
+
+Características 🎯
+Jogo de cobra totalmente funcional.
+Pontuação e cronômetro.
+Suporta reprodução de música personalizada.
+Responsivo para dispositivos móveis.
+Gráficos modernos e atraentes.
+Instruções de Jogo 🎮
+Use as teclas de seta do teclado para mover a cobra ao redor do tabuleiro. Tente comer as maçãs que aparecem aleatoriamente no tabuleiro. Cada maçã que você come aumenta a sua pontuação em 1 ponto e também o tamanho da cobra. Se a cobra se chocar com a borda do tabuleiro ou com ela mesma, o jogo acaba.
+
+Como Executar 🚀
+Baixe ou clone o repositório no seu computador e abra o arquivo index.html em qualquer navegador moderno para jogar.
+
+Contribuição 💡
+Se você quiser contribuir, por favor, faça um fork do repositório e use uma branch de feature. Pull requests são bem vindos.
+
+Licença 📝
+Este projeto está sob a licença MIT. Veja o arquivo LICENSE para mais detalhes.
---
+++ game/game.js
@@ -0,0 +1,138 @@
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+
+var grid = 16;
+var score = 0;
+var timer = 0;
+var interval = null;
+var count = 0;
+
+var snake = {
+    x: 160,
+    y: 160,
+    dx: grid,
+    dy: 0,
+    cells: [],
+    maxCells: 4
+};
+
+var apple = {
+    x: 320,
+    y: 320
+};
+var appleImage = new Image();
+appleImage.src = '../assets/apple.png';
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function updateScore() {
+    document.getElementById('score').innerText = 'Score: ' + score;
+}
+
+function updateTime() {
+    timer++;
+    var minutes = Math.floor(timer / 60);
+    var seconds = timer - minutes * 60;
+    document.getElementById('time').innerText = 'Time: ' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+}
+
+interval = setInterval(updateTime, 1000);
+
+function resetGame() {
+    snake.x = 160;
+    snake.y = 160;
+    snake.cells = [];
+    snake.maxCells = 4;
+    snake.dx = grid;
+    snake.dy = 0;
+    apple.x = getRandomInt(0, 25) * grid;
+    apple.y = getRandomInt(0, 25) * grid;
+    score = 0;
+    updateScore();
+    timer = 0;
+    clearInterval(interval);
+    interval = setInterval(updateTime, 1000);
+}
+
+function loop() {
+    requestAnimationFrame(loop);
+    if (++count < 5) {
+        return;
+    }
+
+    count = 0;
+    context.clearRect(0,0,canvas.width,canvas.height);
+
+    snake.x += snake.dx;
+    snake.y += snake.dy;
+
+    if (snake.x < 0 || snake.y < 0 || snake.x >= canvas.width || snake.y >= canvas.height) {
+        resetGame();
+        return;
+    }
+
+    snake.cells.unshift({x: snake.x, y: snake.y});
+
+    if (snake.cells.length > snake.maxCells) {
+        snake.cells.pop();
+    }
+
+    
+    context.drawImage(appleImage, apple.x, apple.y, grid*1.1-1, grid*1.1-1);
+
+
+    context.fillStyle = 'green';
+    snake.cells.forEach(function(cell, index) {
+        context.fillRect(cell.x, cell.y, grid-1, grid-1);  
+        if (cell.x === apple.x && cell.y === apple.y) {
+            snake.maxCells++;
+            score++;
+            updateScore();
+            apple.x = getRandomInt(0, 25) * grid;
+            apple.y = getRandomInt(0, 25) * grid;
+        }
+        for (var i = index + 1; i < snake.cells.length; i++) {
+            if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+                resetGame();
+                return;
+            }
+        }
+    });
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.which === 37 && snake.dx === 0) {
+        snake.dx = -grid;
+        snake.dy = 0;
+    }
+    else if (e.which === 38 && snake.dy === 0) {
+        snake.dy = -grid;
+        snake.dx = 0;
+    }
+    else if (e.which === 39 && snake.dx === 0) {
+        snake.dx = grid;
+        snake.dy = 0;
+    }
+    else if (e.which === 40 && snake.dy === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+    }
+});
+
+document.getElementById('audio-file').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    const url = URL.createObjectURL(file);
+    const audio = document.getElementById('audio');
+    audio.src = url;
+    audio.onloadeddata = function() {
+        audio.volume = 0.2; // set volume after audio is loaded
+        audio.play(); // play audio after it's loaded
+    };
+});
+
+
+
+
+requestAnimationFrame(loop);
